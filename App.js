ec541da6a36253f8055b8b93488258d5613dcdff
Ishaq Ashraf <ishaqashraf@Ishaqs-MacBook-Pro.local>
2019-08-12 21:10:45 +0500

    splash screen added)natively)

--- App.js
+++ App.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import Navigator from './src/navigation';
 import { Provider } from 'react-redux';
 import Store from './src/store'
+import SplashScreen from 'react-native-splash-screen'
 
 export default class App extends Component {
+
+  componentDidMount(){
+    SplashScreen.hide();
+  }
+
   render() {
     return (
       <Provider store={Store}>
